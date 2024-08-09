60ab7ae5e5921bbc997f4653f5454d3b1b1c5d54
Natthaphon Ditthaviboon <88504995+Bug9Best@users.noreply.github.com>
2024-08-09 22:27:12 +0700

    create component and modules

---
+++ src/app/pages/project/project-gantt/project-gantt.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-project-gantt',
+  standalone: true,
+  imports: [],
+  templateUrl: './project-gantt.component.html',
+  styleUrl: './project-gantt.component.scss'
+})
+export class ProjectGanttComponent {
+
+}
---
+++ src/app/pages/project/project-workspace/project-workspace.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-project-workspace',
+  standalone: true,
+  imports: [],
+  templateUrl: './project-workspace.component.html',
+  styleUrl: './project-workspace.component.scss'
+})
+export class ProjectWorkspaceComponent {
+
+}
