273b18852230da8200f75e00f9cfc4e306edf16c
Natthaphon Ditthaviboon <88504995+Bug9Best@users.noreply.github.com>
2024-08-12 19:33:02 +0700

    import module

--- src/app/template/phantom-dialog/phantom-dialog.component.ts
+++ src/app/template/phantom-dialog/phantom-dialog.component.ts
@@ -1,9 +1,14 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { PrimeNGModule } from '../../shared/modules/primeng.module';
 
 @Component({
   selector: 'app-phantom-dialog',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    PrimeNGModule
+  ],
   templateUrl: './phantom-dialog.component.html',
   styleUrl: './phantom-dialog.component.scss'
 })
--- src/app/template/phantom-form/phantom-form.component.ts
+++ src/app/template/phantom-form/phantom-form.component.ts
@@ -1,9 +1,17 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { PrimeNGModule } from '../../shared/modules/primeng.module';
 
 @Component({
   selector: 'app-phantom-form',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    PrimeNGModule
+  ],
   templateUrl: './phantom-form.component.html',
   styleUrl: './phantom-form.component.scss'
 })
