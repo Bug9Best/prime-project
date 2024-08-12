e0c2d0a13217fa36a374623e7ddcf851ff1f5dcf
Natthaphon Ditthaviboon <88504995+Bug9Best@users.noreply.github.com>
2024-08-13 01:32:36 +0700

    update topbar

--- src/app/template/phantom-page/phantom-topbar/phantom-topbar.component.ts
+++ src/app/template/phantom-page/phantom-topbar/phantom-topbar.component.ts
@@ -1,21 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { PhantomPageService } from '../phantom-page.service';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { MenuModule } from 'primeng/menu';
+import { MenuItem } from 'primeng/api';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'phantom-topbar',
   standalone: true,
   imports: [
     CommonModule,
-    RouterModule
+    RouterModule,
+    MenuModule,
   ],
   templateUrl: './phantom-topbar.component.html',
 })
+
 export class PhantomTopbarComponent {
 
+  languageItems: MenuItem[] = [
+    { label: 'English', command: () => { this.changeLanguage('en'); } },
+    { label: 'ภาษาไทย', command: () => { this.changeLanguage('th'); } },
+  ];
+
   constructor(
-    public phantomPageService: PhantomPageService
-  ) { }
+    public phantomPageService: PhantomPageService,
+  ) {
+  }
+
+  translateService = inject(TranslateService);
+  changeLanguage(lang: string) {
+    // this.translateService.use(lang);
+  }
 
 }
