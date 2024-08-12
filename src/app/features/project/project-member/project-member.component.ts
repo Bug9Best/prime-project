ce95664bdbee375658eea056a1b2a963f8fa6208
Natthaphon Ditthaviboon <88504995+Bug9Best@users.noreply.github.com>
2024-08-12 15:58:15 +0700

    create component

---
+++ src/app/features/project/project-member/project-member.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-project-member',
+  standalone: true,
+  imports: [],
+  templateUrl: './project-member.component.html',
+  styleUrl: './project-member.component.scss'
+})
+export class ProjectMemberComponent {
+
+}
