d1e578f9f3418564abd943084c67cf3d1e9bdc7f
Natthaphon Ditthaviboon <88504995+Bug9Best@users.noreply.github.com>
2024-08-12 19:47:38 +0700

    remove square backet

--- src/app/config/menu.ts
+++ src/app/config/menu.ts
@@ -4,13 +4,13 @@ export var ConfigMenu = [
         icon: 'pi-chevron-down',
         expanded: true,
         items: [
-            { label: 'MENU_DASHBOARD', icon: 'pi-th-large', routerLink: ['/project/dashboard'] },
-            { label: 'MENU_GANTT', icon: 'pi-calendar-clock', routerLink: ['/project/gantt'] },
-            { label: 'MENU_SPRINT', icon: 'pi-spinner', routerLink: ['/project/sprint'] },
-            { label: 'MENU_BACKLOG', icon: 'pi-align-left', routerLink: ['/project/backlog'] },
-            { label: 'MENU_LIST', icon: 'pi-list-check', routerLink: ['/project/issue'] },
-            { label: 'MENU_BOARD', icon: 'pi-clipboard', routerLink: ['/project/board'] },
-            { label: 'MENU_DOCUMENT', icon: 'pi-file', routerLink: ['/project/resource'] },
+            { label: 'MENU_DASHBOARD', icon: 'pi-th-large', routerLink: '/project/dashboard' },
+            { label: 'MENU_GANTT', icon: 'pi-calendar-clock', routerLink: '/project/gantt' },
+            { label: 'MENU_SPRINT', icon: 'pi-spinner', routerLink: '/project/sprint' },
+            { label: 'MENU_BACKLOG', icon: 'pi-align-left', routerLink: '/project/backlog' },
+            { label: 'MENU_LIST', icon: 'pi-list-check', routerLink: '/project/issue' },
+            { label: 'MENU_BOARD', icon: 'pi-clipboard', routerLink: '/project/board' },
+            { label: 'MENU_DOCUMENT', icon: 'pi-file', routerLink: '/project/resource' },
         ]
     },
     {
@@ -18,7 +18,7 @@ export var ConfigMenu = [
         icon: 'pi-chevron-down',
         expanded: true,
         items: [
-            { label: 'MENU_PROJECT_MEMBER', icon: 'pi-users', routerLink: ['/project/member'] },
+            { label: 'MENU_PROJECT_MEMBER', icon: 'pi-users', routerLink: '/project/member' },
             // { label: 'MENU_PROJECT_SETTING', icon: 'pi-cog', routerLink: ['/project-setting'] },
         ]
     },
