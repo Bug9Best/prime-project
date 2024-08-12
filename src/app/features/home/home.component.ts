936489d83f2dde31cd2b88473eedb14c860f8755
Natthaphon Ditthaviboon <88504995+Bug9Best@users.noreply.github.com>
2024-08-13 01:31:24 +0700

    updaate home

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { PrimeNGModule } from '../../shared/modules/primeng.module';
+import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'app-home',
@@ -7,5 +8,49 @@ import { PrimeNGModule } from '../../shared/modules/primeng.module';
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+
+  items: MenuItem[] | undefined;
+
+  position: any = 'bottom';
+
+  positionOptions = [
+    {
+      label: 'Bottom',
+      value: 'bottom'
+    },
+    {
+      label: 'Top',
+      value: 'top'
+    },
+    {
+      label: 'Left',
+      value: 'left'
+    },
+    {
+      label: 'Right',
+      value: 'right'
+    }
+  ];
+
+  ngOnInit() {
+    this.items = [
+      {
+        label: 'Finder',
+        icon: 'https://primefaces.org/cdn/primeng/images/dock/finder.svg'
+      },
+      {
+        label: 'App Store',
+        icon: 'https://primefaces.org/cdn/primeng/images/dock/appstore.svg'
+      },
+      {
+        label: 'Photos',
+        icon: 'https://primefaces.org/cdn/primeng/images/dock/photos.svg'
+      },
+      {
+        label: 'Trash',
+        icon: 'https://primefaces.org/cdn/primeng/images/dock/trash.png'
+      }
+    ];
+  }
 
 }
