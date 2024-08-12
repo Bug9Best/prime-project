b4b63a7fd74539ec5705fc9f2cb7e01969a52ce9
Natthaphon Ditthaviboon <88504995+Bug9Best@users.noreply.github.com>
2024-08-12 19:09:48 +0700

    import module

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,7 @@
 import { Routes } from '@angular/router';
+import { PhantomPage } from './template/phantom-page/phantom-page.component';
+import { ProjectMemberComponent } from './features/project/project-member/project-member.component';
+import { ProjectDashboardComponent } from './features/project/project-dashboard/project-dashboard.component';
 
 export const routes: Routes = [
     {
@@ -12,6 +15,42 @@ export const routes: Routes = [
     },
     {
         path: 'project',
-        loadChildren: () => import('./pages/project/project.module').then(m => m.ProjectModule)
+        component: PhantomPage,
+        children: [
+            {
+                path: 'dashboard',
+                loadChildren: () => import('./features/project/project-dashboard/project-dashboard.module').then(m => m.ProjectDashboardModule)
+            },
+            {
+                path: 'sprint',
+                loadChildren: () => import('./features/project/project-sprint/project-sprint.module').then(m => m.ProjectSprintModule)
+            },
+            {
+                path: 'backlog',
+                loadChildren: () => import('./features/project/project-backlog/project-backlog.module').then(m => m.ProjectBacklogModule)
+            },
+            {
+                path: 'issue',
+                loadChildren: () => import('./features/project/project-issue/project-issue.module').then(m => m.ProjectIssueModule)
+            },
+            {
+                path: 'board',
+                loadChildren: () => import('./features/project/project-board/project-board.module').then(m => m.ProjectBoardModule)
+            },
+            {
+                path: 'gantt',
+                loadChildren: () => import('./features/project/project-gantt/project-gantt.module').then(m => m.ProjectGanttModule)
+            },
+            {
+                path: 'resource',
+                loadChildren: () => import('./features/project/project-resource/project-resource.module').then(m => m.ProjectResourceModule)
+            },
+            {
+                path: 'member',
+                loadChildren: () => import('./features/project/project-member/project-member.module').then(m => m.ProjectMemberModule)
+            },
+        
+        ]
+        // 
     },
 ];
