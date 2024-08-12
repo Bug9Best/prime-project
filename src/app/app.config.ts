ef00660869f2bad6f42761a7a24ac1c6b16a250c
Natthaphon Ditthaviboon <88504995+Bug9Best@users.noreply.github.com>
2024-08-12 16:42:11 +0700

    fixed bugs

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,7 @@ import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } fr
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, provideHttpClient, withFetch } from '@angular/common/http';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -10,7 +10,7 @@ import { ConfirmationService, MessageService } from 'primeng/api';
 import { BrowserModule } from '@angular/platform-browser';
 
 export function HttpLoaderFactory(http: HttpClient): TranslateHttpLoader {
-  return new TranslateHttpLoader(http, '/public/i18n/', '.json');
+  return new TranslateHttpLoader(http, '/i18n/', '.json');
 }
 
 export const appConfig: ApplicationConfig = {
@@ -28,6 +28,7 @@ export const appConfig: ApplicationConfig = {
         }
       })
     ]),
+    provideHttpClient(withFetch()),
     MessageService,
     ConfirmationService,
   ]
