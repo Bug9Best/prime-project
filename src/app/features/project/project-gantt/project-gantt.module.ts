4d1b7e7cb9d977eb01f2f57be28300e83a04bbb3
Natthaphon Ditthaviboon <88504995+Bug9Best@users.noreply.github.com>
2024-08-13 13:22:01 +0700

    import module

--- src/app/features/project/project-gantt/project-gantt.module.ts
+++ src/app/features/project/project-gantt/project-gantt.module.ts
@@ -4,6 +4,9 @@ import { RouterModule, Routes } from '@angular/router';
 import { TranslateModule } from '@ngx-translate/core';
 import { PrimeNGModule } from '../../../shared/modules/primeng.module';
 import { ProjectGanttComponent } from './project-gantt.component';
+import { GanttPrintService, NgxGanttModule } from '@worktile/gantt';
+import { ThyButtonModule } from 'ngx-tethys/button';
+import { GanttConfigProvider } from '../../../config/gantt.config';
 
 const routes: Routes = [
   {
@@ -21,6 +24,10 @@ const routes: Routes = [
     RouterModule.forChild(routes),
     PrimeNGModule,
     TranslateModule,
-  ]
+    NgxGanttModule,
+    ThyButtonModule
+  ],
+  providers: [GanttPrintService, GanttConfigProvider]
+
 })
 export class ProjectGanttModule { }
