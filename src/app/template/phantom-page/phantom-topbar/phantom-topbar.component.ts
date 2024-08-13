ae4ccd05f4729c9ff6b4f397f5f32db1cb388f0a
Natthaphon Ditthaviboon <88504995+Bug9Best@users.noreply.github.com>
2024-08-13 10:42:55 +0700

    done change theme and language

--- src/app/template/phantom-page/phantom-topbar/phantom-topbar.component.ts
+++ src/app/template/phantom-page/phantom-topbar/phantom-topbar.component.ts
@@ -1,8 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, inject, ViewChild } from '@angular/core';
 import { PhantomPageService } from '../phantom-page.service';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-import { MenuModule } from 'primeng/menu';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { OverlayPanelModule } from 'primeng/overlaypanel';
+import { ButtonModule } from 'primeng/button';
+import { Overlay } from 'primeng/overlay';
 
 @Component({
   selector: 'phantom-topbar',
@@ -10,15 +13,32 @@ import { MenuModule } from 'primeng/menu';
   imports: [
     CommonModule,
     RouterModule,
-    MenuModule,
+    TranslateModule,
+    OverlayPanelModule,
+    ButtonModule
   ],
-  templateUrl: './phantom-topbar.component.html',
+  templateUrl: './phantom-topbar.component.html'
 })
 
 export class PhantomTopbarComponent {
+  isLightTheme: boolean = true;
 
   constructor(
     public phantomPageService: PhantomPageService,
   ) {
+  }
+
+  @ViewChild('op', { static: false }) overlay!: Overlay;
+  translateService = inject(TranslateService);
+  changeLanguage(lang: string) {
+    this.translateService.use(lang);
+    this.overlay.hide();
+  }
+
+  changeTheme(state: boolean) {
+    let activeTheme = (state) ? 'lara-dark-blue' : 'lara-light-blue';
+    let themeLink = <HTMLLinkElement>document.getElementById('app-theme');
+    themeLink.href = `/themes/${activeTheme}/theme.css`;
+    this.isLightTheme = !this.isLightTheme;
   }
 }
