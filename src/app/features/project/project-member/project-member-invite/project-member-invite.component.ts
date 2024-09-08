e53341cc674766e8591407e7b1d30c399a2115dd
Natthaphon Ditthaviboon <88504995+Bug9Best@users.noreply.github.com>
2024-09-08 12:15:00 +0700

    done member invite component

---
+++ src/app/features/project/project-member/project-member-invite/project-member-invite.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'project-member-invite',
+  templateUrl: './project-member-invite.component.html',
+  styleUrl: './project-member-invite.component.scss'
+})
+export class ProjectMemberInvite {
+
+  isAddMember = false;
+
+}
