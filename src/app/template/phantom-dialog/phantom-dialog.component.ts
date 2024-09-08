3693d2db026844c00323c076ca39443a68217990
Natthaphon Ditthaviboon <88504995+Bug9Best@users.noreply.github.com>
2024-09-08 12:08:33 +0700

    create new dialog template component

--- src/app/template/phantom-dialog/phantom-dialog.component.ts
+++ src/app/template/phantom-dialog/phantom-dialog.component.ts
@@ -1,6 +1,4 @@
-import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
-import { PrimeNGModule } from '../../shared/modules/primeng.module';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'phantom-dialog',
@@ -9,4 +7,105 @@ import { PrimeNGModule } from '../../shared/modules/primeng.module';
 })
 export class PhantomDialog {
 
+  @Input()
+  data: any;
+
+  @Input()
+  visible: boolean = false;
+
+  @Input()
+  showButton: boolean = true;
+
+  @Input()
+  icon: string = "";
+
+  @Input()
+  buttonStyle: any = {};
+
+  @Input()
+  buttonStyleClass: string = "p-button-sm";
+
+  @Input()
+  label: string = "BUTTON";
+
+  @Input()
+  modal: boolean = true;
+
+  @Input()
+  appendTo: string = 'body';
+
+  @Input()
+  draggable: boolean = false;
+
+  @Input()
+  dismissableMask: boolean = true;
+
+  @Input()
+  dialogWidth: string = "auto";
+
+  @Input()
+  dialogHeight: string = "auto";
+
+  @Input()
+  dialogstyle: any = { minWidth: '50vw' };
+
+  @Input()
+  showEdit: boolean = false;
+
+  @Input()
+  showTrash: boolean = false;
+
+  @Input()
+  showClose: boolean = true;
+
+  @Input()
+  showBack: boolean = false;
+
+  @Input()
+  header?: string;
+
+  @Input()
+  subHeader?: string;
+
+  @Input()
+  showHeader: boolean = true;
+
+  @Input()
+  showSubHeader: boolean = true;
+
+  @Input()
+  headerStyleClass: string = "p-0";
+
+  @Input()
+  contentStyleClass: string = "p-4";
+
+  @Output()
+  visibleChange: EventEmitter<boolean> = new EventEmitter<boolean>();
+
+  @Output()
+  onCloseClick: EventEmitter<void> = new EventEmitter();
+
+  @Output()
+  onEditClick: EventEmitter<any> = new EventEmitter();
+
+  @Output()
+  onTrashClick: EventEmitter<any> = new EventEmitter();
+
+  @Output()
+  onBackClick: EventEmitter<any> = new EventEmitter();
+
+  onShow(): void {
+    this.visible = true;
+    this.visibleChange.emit(this.visible);
+  }
+
+  onClose(): void {
+    this.visible = false;
+    this.visibleChange.emit(this.visible);
+    this.onCloseClick.emit();
+  }
+
+  show(): void {
+    this.onShow();
+  }
 }
