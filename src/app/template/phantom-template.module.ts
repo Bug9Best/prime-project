18bb3572e5b4199de5cf2acb64523d26f5a7996a
Natthaphon Ditthaviboon <88504995+Bug9Best@users.noreply.github.com>
2024-09-08 11:30:09 +0700

    update name module

---
+++ src/app/template/phantom-template.module.ts
@@ -0,0 +1,40 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PhantomContainer } from './phantom-container/phantom-container.component';
+import { PhantomDialog } from './phantom-dialog/phantom-dialog.component';
+import { PrimeNGModule } from '../shared/modules/primeng.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { PhantomForm } from './phantom-form/phantom-form.component';
+import { PhantomHeader } from './phantom-header/phantom-header.component';
+import { TranslateModule } from '@ngx-translate/core';
+import { PhantomTable } from './phantom-table/phantom-table.component';
+import { ThaiModule } from '../shared/pipes/thai.module';
+import { PhantomContent } from './phantom-content/phantom-content.component';
+
+@NgModule({
+  declarations: [
+    PhantomContainer,
+    PhantomDialog,
+    PhantomForm,
+    PhantomHeader,
+    PhantomTable,
+    PhantomContent,
+  ],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    PrimeNGModule,
+    ThaiModule,
+    TranslateModule,
+  ],
+  exports: [
+    PhantomContainer,
+    PhantomDialog,
+    PhantomForm,
+    PhantomHeader,
+    PhantomTable,
+    PhantomContent,
+  ],
+})
+export class PhantomTemplateModule {}
