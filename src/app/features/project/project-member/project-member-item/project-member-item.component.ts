eee32f72e0f0e027ec9f85b882ae134792ddc6a4
Natthaphon Ditthaviboon <88504995+Bug9Best@users.noreply.github.com>
2024-09-08 12:40:15 +0700

    refactor code

--- src/app/features/project/project-member/project-member-item/project-member-item.component.ts
+++ src/app/features/project/project-member/project-member-item/project-member-item.component.ts
@@ -1,20 +1,19 @@
-import { Component, Input, output, ViewChild } from '@angular/core';
-import { ProjectMemberDetailComponent } from '../project-member-detail/project-member-detail.component';
+import { Component, Input, ViewChild } from '@angular/core';
+import { ProjectMemberDetail } from '../project-member-detail/project-member-detail.component';
 
 @Component({
   selector: 'project-member-item',
   templateUrl: './project-member-item.component.html',
   styleUrl: './project-member-item.component.scss'
 })
-export class ProjectMemberItemComponent {
+export class ProjectMemberItem {
 
   @Input() member: any;
 
-  @ViewChild(ProjectMemberDetailComponent)
-  memberDetail!: ProjectMemberDetailComponent;
+  @ViewChild(ProjectMemberDetail)
+  memberDetail!: ProjectMemberDetail;
   onSelectMember() {
     this.memberDetail.member = this.member;
     this.memberDetail.isShowMember = !this.memberDetail.isShowMember;
   }
-
 }
