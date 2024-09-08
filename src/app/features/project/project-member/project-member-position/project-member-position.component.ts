109f5e00e8ad53f8367df4fe418f101d2c0de859
Natthaphon Ditthaviboon <88504995+Bug9Best@users.noreply.github.com>
2024-09-08 13:13:53 +0700

    done pisition dialog

--- src/app/features/project/project-member/project-member-position/project-member-position.component.ts
+++ src/app/features/project/project-member/project-member-position/project-member-position.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-project-member-position',
-  standalone: true,
-  imports: [],
+  selector: 'project-member-position',
   templateUrl: './project-member-position.component.html',
   styleUrl: './project-member-position.component.scss'
 })
-export class ProjectMemberPositionComponent {
+export class ProjectMemberPosition {
 
+  isAddPosition: boolean = false;
+
+  positions: any[] = [
+    { id: 1, name: 'Developer' },
+    { id: 2, name: 'Designer' },
+    { id: 3, name: 'Tester' },
+  ];
 }
