a87996ea59f96e8bb4d49781b881ad22a3815985
Natthaphon Ditthaviboon <88504995+Bug9Best@users.noreply.github.com>
2024-09-08 13:15:40 +0700

    update

--- src/app/features/project/project-dashboard/project-task/project-task.component.ts
+++ src/app/features/project/project-dashboard/project-task/project-task.component.ts
@@ -12,8 +12,113 @@ export class ProjectTask {
   });
 
   stateOptions: any[] = [
-    { label: 'All Task', value: 'ALL' },
+    { label: 'All Task', value: 'ALL'},
     { label: 'Critical', value: 'CRITICAL' },
     { label: 'Overdue', value: 'OVER' },
   ];
+
+  listTask: any[] = [
+    {
+      id: 1,
+      title: 'Task 1',
+      status: 'CRITICAL',
+      dueDate: '2021-12-31',
+    },
+    {
+      id: 2,
+      title: 'Task 2',
+      status: 'OVER',
+      dueDate: '2021-12-31',
+    },
+    {
+      id: 3,
+      title: 'Task 3',
+      status: 'NORMAL',
+      dueDate: '2021-12-31',
+    },
+    {
+      id: 4,
+      title: 'Task 4',
+      status: 'NORMAL',
+      dueDate: '2021-12-31',
+    },
+    {
+      id: 5,
+      title: 'Task 5',
+      status: 'NORMAL',
+      dueDate: '2021-12-31',
+    },
+    {
+      id: 6,
+      title: 'Task 6',
+      status: 'NORMAL',
+      dueDate: '2021-12-31',
+    },
+    {
+      id: 7,
+      title: 'Task 7',
+      status: 'NORMAL',
+      dueDate: '2021-12-31',
+    },
+    {
+      id: 8,
+      title: 'Task 8',
+      status: 'NORMAL',
+      dueDate: '2021-12-31',
+    },
+    {
+      id: 9,
+      title: 'Task 9',
+      status: 'NORMAL',
+      dueDate: '2021-12-31',
+    },
+    {
+      id: 10,
+      title: 'Task 10',
+      status: 'NORMAL',
+      dueDate: '2021-12-31',
+    },
+    {
+      id: 11,
+      title: 'Task 11',
+      status: 'NORMAL',
+      dueDate: '2021-12-31',
+    },
+    {
+      id: 12,
+      title: 'Task 12',
+      status: 'NORMAL',
+      dueDate: '2021-12-31',
+    },
+    {
+      id: 13,
+      title: 'Task 13',
+      status: 'NORMAL',
+      dueDate: '2021-12-31',
+    },
+    {
+      id: 14,
+      title: 'Task 14',
+      status: 'NORMAL',
+      dueDate: '2021-12-31',
+    },
+    {
+      id: 15,
+      title: 'Task 15',
+      status: 'NORMAL',
+      dueDate: '2021-12-31',
+    },
+    {
+      id: 16,
+      title: 'Task 16',
+      status: 'NORMAL',
+      dueDate: '2021-12-31',
+    },
+    {
+      id: 17,
+      title: 'Task 17',
+      status: 'NORMAL',
+      dueDate: '2021-12-31 ',
+    }
+  ]
 }
--- src/app/features/project/project-member/project-member-detail/project-member-detail.component.ts
+++ src/app/features/project/project-member/project-member-detail/project-member-detail.component.ts
@@ -1,12 +1,26 @@
-import { Component, Input, ViewChild } from '@angular/core';
+import { Component } from '@angular/core';
+import { ConfirmationService } from 'primeng/api';
 
 @Component({
   selector: 'project-member-detail',
   templateUrl: './project-member-detail.component.html',
   styleUrl: './project-member-detail.component.scss'
 })
-export class ProjectMemberDetailComponent {
+export class ProjectMemberDetail {
 
   member: any;
   isShowMember: boolean = false;
+
+  constructor(
+    private confirmationService: ConfirmationService,
+  ) { }
+
+  removeMember(item: any) {
+    this.confirmationService.confirm({
+      message: 'Do you want to remove this member from project?',
+      accept: () => {
+        this.isShowMember = false;
+      }
+    });
+  }
 }
