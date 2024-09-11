b018e66121f90628704b1d0542c4c852eb6edaed
Bug9Best <bug9best@gmail.com>
2024-09-11 19:00:57 +0700

    add board

--- src/app/features/project/project-board/project-board-add/project-board-add.component.ts
+++ src/app/features/project/project-board/project-board-add/project-board-add.component.ts
@@ -1,6 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, output } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
-import { KanbanBoard } from '../../../../shared/data/board';
+import { MessageService } from 'primeng/api';
 
 @Component({
   selector: 'project-board-add',
@@ -8,14 +8,41 @@ import { KanbanBoard } from '../../../../shared/data/board';
   styleUrl: './project-board-add.component.scss',
 })
 export class ProjectBoardAdd {
-  isAddBoard: boolean = false;
+  visible: boolean = false;
 
   formGroup: FormGroup = new FormGroup({
-    name: new FormControl('', Validators.required),
+    name: new FormControl(null, Validators.required),
   });
 
-  closeDialog() {
+  constructor(private messageService: MessageService) {}
+
+  resetForm() {
     this.formGroup.reset();
-    this.isAddBoard = false;
+    this.visible = false;
+  }
+
+  showMessages(severity: string, summary: string, detail: string) {
+    this.messageService.add({
+      key: 'app',
+      severity: severity,
+      summary: summary,
+      detail: detail,
+    });
+  }
+  validateForm() {
+    if (this.formGroup.invalid) {
+      this.formGroup.markAllAsTouched();
+      this.showMessages('warn', 'Error', 'Please fill in the form');
+      return;
+    }
+    this.onCreateBoard();
+  }
+
+  onCreateEvent = output<any>();
+  onCreateBoard() {
+    let values = this.formGroup.value;
+    this.onCreateEvent.emit(values);
+    this.showMessages('success', 'Success', 'Board created successfully');
+    this.resetForm();
   }
 }
