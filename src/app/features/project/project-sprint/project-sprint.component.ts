67e82d84c2803293cb694d7537d15779087e827c
Bug9Best <bug9best@gmail.com>
2024-09-11 16:57:55 +0700

    update main component of project sprint

--- src/app/features/project/project-sprint/project-sprint.component.ts
+++ src/app/features/project/project-sprint/project-sprint.component.ts
@@ -1,8 +1,4 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { MessageService, TreeNode } from 'primeng/api';
-import { SprintData } from '../../../shared/service/sprintdb';
-import { PhantomTableForlistColumn } from '../../../template/phantom-table-for-list/phantom-table-for-list.component';
 
 @Component({
   selector: 'app-project-sprint',
