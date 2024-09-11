de57d5e3699d2305f2e500f581d0ccef35816e56
Bug9Best <bug9best@gmail.com>
2024-09-11 13:31:37 +0700

    done component create resource

--- src/app/features/project/project-resource/project-resource-create/project-resource-create.component.ts
+++ src/app/features/project/project-resource/project-resource-create/project-resource-create.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, inject, output, Output, signal } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { MessageService } from 'primeng/api';
 
 @Component({
   selector: 'project-resource-create',
   templateUrl: './project-resource-create.component.html',
-  styleUrl: './project-resource-create.component.scss'
+  styleUrl: './project-resource-create.component.scss',
 })
 export class ProjectResourceCreate {
+  visible: boolean = false;
 
+  text = signal<string>('');
+
+  formGroup: FormGroup = new FormGroup({
+    topic: new FormControl(null, Validators.required),
+    content: new FormControl(null),
+  });
+
+  onCloseDialog() {
+    this.formGroup.reset();
+  }
+
+  messageService = inject(MessageService);
+  showMessage(severity: string, summary: string, detail: string) {
+    this.messageService.add({
+      key: 'app',
+      severity: severity,
+      summary: summary,
+      detail: detail,
+    });
+  }
+
+  onAddResource = output<any>();
+  validateForm(): void {
+    if (this.formGroup.invalid) {
+      this.formGroup.controls['topic'].markAsDirty();
+      this.showMessage('warn', 'Error', 'Please fill in all required fields');
+      return;
+    }
+
+    let values = this.formGroup.value;
+    this.onAddResource.emit(values);
+  }
 }
