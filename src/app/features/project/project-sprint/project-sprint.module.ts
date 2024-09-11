690addece59ceb1f1b965dfd94d3bb0b79b8b24b
Bug9Best <bug9best@gmail.com>
2024-09-11 17:48:42 +0700

    remove unuse

--- src/app/features/project/project-sprint/project-sprint.module.ts
+++ src/app/features/project/project-sprint/project-sprint.module.ts
@@ -4,13 +4,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { ProjectSprintComponent } from './project-sprint.component';
 import { PrimeNGModule } from '../../../shared/modules/primeng.module';
 import { TranslateModule } from '@ngx-translate/core';
-import { DividerModule } from 'primeng/divider';
-import { TreeTableModule } from 'primeng/treetable';
-import { CheckboxModule } from 'primeng/checkbox';
 import { ReactiveFormsModule } from '@angular/forms';
-import { ThyButton, ThyButtonModule } from 'ngx-tethys/button';
-import { ThyLayoutModule } from 'ngx-tethys/layout';
-import { NgxGanttModule } from '@worktile/gantt';
 import { PhantomTemplateModule } from '../../../template/phantom-template.module';
 import { ProjectSprintCreate } from './project-sprint-create/project-sprint-create.component';
 import { ProjectSprintList } from './project-sprint-list/project-sprint-list.component';
@@ -33,17 +27,8 @@ const routes: Routes = [
     RouterModule.forChild(routes),
     PrimeNGModule,
     TranslateModule,
-    DividerModule,
-    TreeTableModule,
-    CheckboxModule,
-    TreeTableModule,
     ReactiveFormsModule,
-    ThyButtonModule,
-    ThyButton,
-    ThyLayoutModule,
-    NgxGanttModule,
     PhantomTemplateModule,
-    TranslateModule
   ]
 })
 export class ProjectSprintModule { }
