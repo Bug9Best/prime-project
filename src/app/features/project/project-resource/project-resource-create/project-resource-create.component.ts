28985b8d62422d603c3d8d6cef763c4f3d771eb3
Bug9Best <bug9best@gmail.com>
2024-09-11 10:50:31 +0700

    create new component for create resource

---
+++ src/app/features/project/project-resource/project-resource-create/project-resource-create.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'project-resource-create',
+  templateUrl: './project-resource-create.component.html',
+  styleUrl: './project-resource-create.component.scss'
+})
+export class ProjectResourceCreate {
+
+}
