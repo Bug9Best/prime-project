deb58a9f4180c5efcc5e480fb517ff5e9ddc0b45
Natthaphon Ditthaviboon <88504995+Bug9Best@users.noreply.github.com>
2024-09-18 13:46:04 +0700

    update

--- src/app/shared/data/issue.ts
+++ src/app/shared/data/issue.ts
@@ -1,13 +1,15 @@
 
 export var listIssue = [
     {
-        "title": 1,
+        "id": 1,
+        "title": "Task 1",
         "type": "Issue 1",
         "sprint": "Description for Issue 1",
         "assignee": "Phuri"
     },
     {
-        "title": 2,
+        "id": 2,
+        "title": "Task 2",
         "type": "Issue 2",
         "sprint": "Description for Issue 2",
         "assignee": "Phuri Siripool"
