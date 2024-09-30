cb955ac6d34b195be96368d2084fb5c1c164d5f2
Bug9Best <bug9best@gmail.com>
2024-10-01 06:41:50 +0700

    create new compone t for attachment and link

---
+++ src/app/features/project/project-resource/create-resource-link/create-resource-link.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'create-resource-link',
+  templateUrl: './create-resource-link.component.html',
+  styleUrl: './create-resource-link.component.scss'
+})
+export class CreateResourceLinkComponent {
+
+}
