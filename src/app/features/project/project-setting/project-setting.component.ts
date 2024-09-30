8b10d236c61cc8b01b99a600581556f31321378e
Phuri Siripool <85283698+Phuriq@users.noreply.github.com>
2024-10-01 06:40:01 +0700

    PROJECT-SETTINGS-UP

--- src/app/features/project/project-setting/project-setting.component.ts
+++ src/app/features/project/project-setting/project-setting.component.ts
@@ -1,10 +1,39 @@
 import { Component } from '@angular/core';
+import { MessageService } from 'primeng/api';
+import { UploadEvent } from 'primeng/fileupload';
 
 @Component({
   selector: 'app-project-setting',
   templateUrl: './project-setting.component.html',
-  styleUrl: './project-setting.component.scss'
+  styleUrl: './project-setting.component.scss',
 })
 export class ProjectSettingComponent {
+  member: any[] = [];
 
+  ngOnInit() {
+    this.member = [{ name: 'Phuri siripool' }, { name: 'Natthapong D. Best' }];
+  }
+
+  constructor(private messageService: MessageService) {}
+  title: string = 'Project Test';
+  description: string = 'Description Test';
+
+  TitleisEditing: boolean = false;
+  DescriptionisEditing: boolean = false;
+
+  toggleTitleEdit() {
+    this.TitleisEditing = !this.TitleisEditing;
+  }
+
+  toggleDescriptionEdit() {
+    this.DescriptionisEditing = !this.DescriptionisEditing;
+  }
+  
+  onBasicUploadAuto(event: UploadEvent) {
+    this.messageService.add({
+      severity: 'info',
+      summary: 'Success',
+      detail: 'File Uploaded with Auto Mode',
+    });
+  }
 }
