a1b03b459205072bd15e2ea0ee87c30c2b1b2387
Bug9Best <bug9best@gmail.com>
2024-10-01 04:43:30 +0700

    add resource type options

---
+++ src/app/shared/options/resource-type.ts
@@ -0,0 +1,20 @@
+export var ResourceType = [
+  {
+    value: 1,
+    title: 'Content',
+    description: 'Create a new content resource',
+    imageSrc: 'images/assets/blog-pencil.png',
+  },
+  {
+    value: 2,
+    title: 'External Link',
+    description: 'Create a new external link resource',
+    imageSrc: 'images/assets/web-link.png',
+  },
+  {
+    value: 3,
+    title: 'Attachments',
+    description: 'Create a new attachment resource',
+    imageSrc: 'images/assets/attached-file.png',
+  },
+];
