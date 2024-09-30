9a4faac31fbedc596a310dcdadfedfaf42ededaf
Bug9Best <bug9best@gmail.com>
2024-10-01 06:41:25 +0700

    done project resource selection component

--- src/app/features/project/project-resource/project-resource-selection/project-resource-selection.component.ts
+++ src/app/features/project/project-resource/project-resource-selection/project-resource-selection.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, inject, output } from '@angular/core';
+import { MessageService } from 'primeng/api';
+import { ResourceType } from '../../../../shared/options/resource-type';
 
 @Component({
   selector: 'project-resource-selection',
   templateUrl: './project-resource-selection.component.html',
-  styleUrl: './project-resource-selection.component.scss'
+  styleUrl: './project-resource-selection.component.scss',
 })
 export class ProjectResourceSelectionComponent {
+  
+  selectedType: number = 0;
+  visible: boolean = false;
+  listType: any[] = ResourceType;
 
+  onCloseDialog(visible: boolean) {
+    this.visible = visible;
+    this.resetSelection();
+  }
+
+  resetSelection() {
+    this.selectedType = 0;
+  }
+
+  messageService = inject(MessageService);
+  showMessage(severity: string, summary: string, detail: string) {
+    this.messageService.add({
+      key: 'app',
+      severity: severity,
+      summary: summary,
+      detail: detail,
+    });
+  }
+
+  onSelectTypeEvent = output<any>();
+  validateState() {
+    if (!this.selectedType) return;
+    this.onSelectTypeEvent.emit(this.selectedType);
+    this.onCloseDialog(false);
+  }
 }
