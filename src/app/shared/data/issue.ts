c9169414be8ae1a92d67982dbc57fcea827fd958
Bug9Best <bug9best@gmail.com>
2024-10-01 02:00:36 +0700

    add mock up issue

--- src/app/shared/data/issue.ts
+++ src/app/shared/data/issue.ts
@@ -14,4 +14,200 @@ export var listIssue = [
         "sprint": "Description for Issue 2",
         "assignee": "Phuri Siripool"
     },
+    {
+        "id": 3,
+        "title": "Task 3",
+        "type": "Issue 3",
+        "sprint": "Description for Issue 3",
+        "assignee": "Phuri Siripool"
+    },
+    {
+        "id": 4,
+        "title": "Task 4",
+        "type": "Issue 4",
+        "sprint": "Description for Issue 4",
+        "assignee": "Phuri Siripool"
+    },
+    {
+        "id": 5,
+        "title": "Task 5",
+        "type": "Issue 5",
+        "sprint": "Description for Issue 5",
+        "assignee": "Phuri Siripool"
+    },
+    {
+        "id": 6,
+        "title": "Task 6",
+        "type": "Issue 6",
+        "sprint": "Description for Issue 6",
+        "assignee": "Phuri Siripool"
+    },
+    {
+        "id": 7,
+        "title": "Task 7",
+        "type": "Issue 7",
+        "sprint": "Description for Issue 7",
+        "assignee": "Phuri Siripool"
+    },
+    {
+        "id": 8,
+        "title": "Task 8",
+        "type": "Issue 8",
+        "sprint": "Description for Issue 8",
+        "assignee": "Phuri Siripool"
+    },
+    {
+        "id": 9,
+        "title": "Task 9",
+        "type": "Issue 9",
+        "sprint": "Description for Issue 9",
+        "assignee": "Phuri Siripool"
+    },
+    {
+        "id": 10,
+        "title": "Task 10",
+        "type": "Issue 10",
+        "sprint": "Description for Issue 10",
+        "assignee": "Phuri Siripool"
+    },
+    {
+        "id": 11,
+        "title": "Task 11",
+        "type": "Issue 11",
+        "sprint": "Description for Issue 11",
+        "assignee": "Phuri Siripool"
+    },
+    {
+        "id": 12,
+        "title": "Task 12",
+        "type": "Issue 12",
+        "sprint": "Description for Issue 12",
+        "assignee": "Phuri Siripool"
+    },
+    {
+        "id": 13,
+        "title": "Task 13",
+        "type": "Issue 13",
+        "sprint": "Description for Issue 13",
+        "assignee": "Phuri Siripool"
+    },
+    {
+        "id": 14,
+        "title": "Task 14",
+        "type": "Issue 14",
+        "sprint": "Description for Issue 14",
+        "assignee": "Phuri Siripool"
+    },
+    {
+        "id": 15,
+        "title": "Task 15",
+        "type": "Issue 15",
+        "sprint": "Description for Issue 15",
+        "assignee": "Phuri Siripool"
+    },
+    {
+        "id": 16,
+        "title": "Task 16",
+        "type": "Issue 16",
+        "sprint": "Description for Issue 16",
+        "assignee": "Phuri Siripool"
+    },
+    {
+        "id": 17,
+        "title": "Task 17",
+        "type": "Issue 17",
+        "sprint": "Description for Issue 17",
+        "assignee": "Phuri Siripool"
+    },
+    {
+        "id": 18,
+        "title": "Task 18",
+        "type": "Issue 18",
+        "sprint": "Description for Issue 18",
+        "assignee": "Phuri Siripool"
+    },
+    {
+        "id": 19,
+        "title": "Task 19",
+        "type": "Issue 19",
+        "sprint": "Description for Issue 19",
+        "assignee": "Phuri Siripool"
+    },
+    {
+        "id": 20,
+        "title": "Task 20",
+        "type": "Issue 20",
+        "sprint": "Description for Issue 20",
+        "assignee": "Phuri Siripool"
+    },
+    {
+        "id": 21,
+        "title": "Task 21",
+        "type": "Issue 21",
+        "sprint": "Description for Issue 21",
+        "assignee": "Phuri Siripool"
+    },
+    {
+        "id": 22,
+        "title": "Task 22",
+        "type": "Issue 22",
+        "sprint": "Description for Issue 22",
+        "assignee": "Phuri Siripool"
+    },
+    {
+        "id": 23,
+        "title": "Task 23",
+        "type": "Issue 23",
+        "sprint": "Description for Issue 23",
+        "assignee": "Phuri Siripool"
+    },
+    {
+        "id": 24,
+        "title": "Task 24",
+        "type": "Issue 24",
+        "sprint": "Description for Issue 24",
+        "assignee": "Phuri Siripool"
+    },
+    {
+        "id": 25,
+        "title": "Task 25",
+        "type": "Issue 25",
+        "sprint": "Description for Issue 25",
+        "assignee": "Phuri Siripool"
+    },
+    {
+        "id": 26,
+        "title": "Task 26",
+        "type": "Issue 26",
+        "sprint": "Description for Issue 26",
+        "assignee": "Phuri Siripool"
+    },
+    {
+        "id": 27,
+        "title": "Task 27",
+        "type": "Issue 27",
+        "sprint": "Description for Issue 27",
+        "assignee": "Phuri Siripool"
+    },
+    {
+        "id": 28,
+        "title": "Task 28",
+        "type": "Issue 28",
+        "sprint": "Description for Issue 28",
+        "assignee": "Phuri Siripool"
+    },
+    {
+        "id": 29,
+        "title": "Task 29",
+        "type": "Issue 29",
+        "sprint": "Description for Issue 29",
+        "assignee": "Phuri Siripool"
+    },
+    {
+        "id": 30,
+        "title": "Task 30",
+        "type": "Issue 30",
+        "sprint": "Description for Issue 30",
+        "assignee": "Phuri Siripool"
+    },
 ]
