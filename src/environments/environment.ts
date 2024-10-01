a166368c5a855fd06ae15eb2f9a9341ecf371818
Bug9Best <bug9best@gmail.com>
2024-10-02 02:52:08 +0700

    add firebase config

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,14 @@
 export const environment = {
-    production: false,
-    baseUrl: "/api",
-    apiUrl: "http://localhost:8080/api/v1",
+  production: false,
+  baseUrl: '/api',
+  apiUrl: 'http://localhost:8080/api/v1',
+  firebaseConfig: {
+    apiKey: 'AIzaSyBvz-0QN5DEZ5qDjXmSV-7mw5m7qjXYLE8',
+    authDomain: 'prime-project-1539.firebaseapp.com',
+    projectId: 'prime-project-1539',
+    locationId: 'asia-southeast1',
+    storageBucket: 'prime-project-1539.appspot.com',
+    messagingSenderId: '361060623041',
+    appId: '1:361060623041:web:05997ed3b6a31da8742a40',
+  },
 };
