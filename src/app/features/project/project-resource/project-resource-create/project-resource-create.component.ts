e0d9671114c611984002c010d3b30356688ebfec
Bug9Best <bug9best@gmail.com>
2024-10-02 02:54:02 +0700

    done create resource component

--- src/app/features/project/project-resource/project-resource-create/project-resource-create.component.ts
+++ src/app/features/project/project-resource/project-resource-create/project-resource-create.component.ts
@@ -1,23 +1,37 @@
 import { Component, inject } from '@angular/core';
 import { MessageService } from 'primeng/api';
 
+export enum ResourceType {
+  SelectType = 1,
+  InputData = 2,
+}
+
 @Component({
   selector: 'project-resource-create',
   templateUrl: './project-resource-create.component.html',
   styleUrl: './project-resource-create.component.scss',
 })
 export class ProjectResourceCreate {
+  currentStep: ResourceType = ResourceType.SelectType;
   typeValue!: number;
   visible: boolean = false;
   text = 'Create Resource';
 
-  setType(data: any) {
-    this.typeValue = data;
-    this.visible = true;
-  }
-
   onCloseDialog(visible: boolean) {
     this.visible = visible;
+    setTimeout(() => {
+      this.currentStep = ResourceType.SelectType;
+    }, 300);
+  }
+
+  onSelectType(data: any) {
+    this.typeValue = data;
+    this.currentStep = ResourceType.InputData;
+  }
+
+  prevStep(state: boolean) {
+    if (!state) return;
+    this.currentStep = ResourceType.SelectType;
   }
 
   messageService = inject(MessageService);
