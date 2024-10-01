b74a0bcdd48d09a597c2ef1f6c92f540da44b506
Bug9Best <bug9best@gmail.com>
2024-10-02 02:53:02 +0700

    done create content component

---
+++ src/app/features/project/project-resource/project-resource-create/create-resource-content/create-resource-content.component.ts
@@ -0,0 +1,69 @@
+import { Component, output } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ConfirmationService, MessageService } from 'primeng/api';
+
+@Component({
+  selector: 'create-resource-content',
+  templateUrl: './create-resource-content.component.html',
+  styleUrl: './create-resource-content.component.scss',
+})
+export class CreateResourceContentComponent {
+  formGroup: FormGroup = new FormGroup({
+    topic: new FormControl(null, Validators.required),
+    content: new FormControl(null, Validators.required),
+    resource_owner: new FormControl(null),
+  });
+
+  constructor(
+    private messageService: MessageService,
+    private confirmationService: ConfirmationService
+  ) {}
+
+  resetForm() {
+    this.formGroup.reset();
+  }
+
+  showMessage(severity: string, summary: string, detail: string) {
+    this.messageService.add({
+      key: 'app',
+      severity: severity,
+      summary: summary,
+      detail: detail,
+    });
+  }
+
+  onValidateForm() {
+    if (this.formGroup.invalid) {
+      this.formGroup.markAllAsTouched();
+      this.showMessage('warn', 'Error', 'Please fill all required fields');
+      return;
+    }
+
+    this.onCreateResource();
+  }
+
+  onBackPreviousStepEvent = output<any>();
+  onBackPreviousStep() {
+    if (this.formGroup.dirty) {
+      this.confirmationService.confirm({
+        header: 'Confirmation',
+        message: 'Are you sure you want to leave this page?',
+        acceptLabel: 'Yes',
+        rejectLabel: 'No',
+        accept: () => {
+          this.resetForm();
+          this.onBackPreviousStepEvent.emit(true);
+        },
+      });
+      return;
+    }
+    this.resetForm();
+    this.onBackPreviousStepEvent.emit(true);
+  }
+
+  onCreateResourceEvent = output<boolean>();
+  onCreateResource() {
+    let values = this.formGroup.value;
+    this.onCreateResourceEvent.emit(values);
+  }
+}
